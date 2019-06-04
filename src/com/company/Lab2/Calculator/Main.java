c7351ff792d6576e09de07fcca37b4d1b4ad94fc
PaulHazi <paul_hazi@yahoo.com>
2019-06-04 19:18:13 +0300

    d

---
+++ src/com/company/Lab2/Calculator/Main.java
@@ -0,0 +1,97 @@
+package com.company.Lab2.Calculator;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printMyName();
+        System.out.println("rezultatul este:" + sum(8, 5));
+        System.out.println("rezultatul este:" + substraction(4, 3));
+        System.out.println("rezultatul este:" + division(4, 2));
+        System.out.println("rezultatul este:" + multiply(4, 5));
+
+        int mult2 = multiply(8, 6);
+        int result = sum(-5, mult2);
+        System.out.println(result);
+        System.out.println(8 * 6 + -5);
+        printFormat1();
+
+        System.out.println("media numerelor este:" + avarageNumber(3, 4, 5));
+        printFormat2();
+
+        System.out.println(remainder(10, 3));
+
+        System.out.println(fahrenheitToCelsius(125));
+        System.out.println(inchToMeters(10.3));
+
+
+    }
+
+    public static int sum(int firstNumber, int secondNumber) {
+        int result = firstNumber + secondNumber;
+        return result;
+    }
+
+    public static int multiply(int firstNumber, int secondNumber) {
+        int result = firstNumber * secondNumber;
+        return result;
+    }
+
+    public static int substraction(int firstNumber, int secondNumber) {
+        int result = firstNumber - secondNumber;
+        return result;
+    }
+
+    public static int division(int firstNumber, int secondNumber) {
+        int result = firstNumber / secondNumber;
+        return result;
+    }
+
+    public static void printMyName() {
+        System.out.println("Hello \n Paul");
+    }
+
+    public static void printFormat1() {
+        System.out.println("    J    a  v     v  a");
+        System.out.println("    J   a a  v   v  a a");
+        System.out.println(" J  J  aaaaa  v v  aaaaa");
+        System.out.println("  JJ  a     a  v  a     a ");
+    }
+
+    public static int avarageNumber(int firstNumber, int secondNumber, int thirdNumber) {
+        int result = (firstNumber + secondNumber + thirdNumber) / 3;
+        return result;
+    }
+
+    public static void printFormat2() {
+        System.out.println("  +\"\"\"\"\"+");
+        System.out.println(" [| O O |]");
+        System.out.println("  |  ^  | ");
+        System.out.println("  |' _ '| ");
+        System.out.println("  +-----+ ");
+    }
+
+    public static double remainder(double firstNumber, double secondNumber) {
+        double result = (firstNumber % secondNumber);
+        return result;
+    }
+
+    public static int fahrenheitToCelsius(int fahrenheit) {
+        int celsius = ((fahrenheit - 32) * 5 / 9);
+        return celsius;
+    }
+
+    public static double inchToMeters(double inch) {
+        double meters = (inch * 0.0254);
+        return meters;
+    }
+
+
+
+}
+
+
+
+
+
