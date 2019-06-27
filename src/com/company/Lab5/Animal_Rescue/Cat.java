1b43eba00ad0839b0a92c4df932747d5671a6303
PaulHazi <paul_hazi@yahoo.com>
2019-06-27 18:49:41 +0300

    hsh

---
+++ src/com/company/Lab5/Animal_Rescue/Cat.java
@@ -0,0 +1,12 @@
+package com.company.Lab5.Animal_Rescue;
+
+public class Cat extends Animal {
+
+    public void meow(){
+        System.out.println("MEOW MEOW");
+    }
+
+
+
+
+}
---
+++ src/com/company/Lab5/Animal_Rescue/Dog.java
@@ -0,0 +1,11 @@
+package com.company.Lab5.Animal_Rescue;
+
+public class Dog extends Animal {
+
+    public void bark (){
+        System.out.println("WooF WooF");
+    }
+
+
+}
+
